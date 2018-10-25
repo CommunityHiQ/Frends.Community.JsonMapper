0527f33671c870201623da6ed0076035160d117b
Jura Laakkonen <Jura.Laakkonen@hiq.fi>
2018-10-25 11:24:15 +0300

    Initial commit oj JsonMapper Task

---
+++ Frends.Community.JsonMapper.Tests/JsonMapperTests.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+
+namespace Frends.Community.JsonMapper.Tests
+{
+    [TestFixture]
+    public class JsonMapperTests
+    {
+        InputProperties _testInput;
+        private const string _testJson =
+@"
+{
+    ""firstName"": ""Veijo"",
+    ""lastName"": ""Frends"",
+    ""age"": 30,
+    ""retired"": false
+}
+";
+        private const string _testMap =
+@"
+{
+    ""FullName"": ""#xconcat(#valueof($.firstName), ,#valueof($.lastName))"",
+    ""Age"" : ""#valueof($.age)"",
+    ""StillBreething"": ""#valueof($.retired)""
+}
+";
+        [SetUp]
+        public void TestSetup()
+        {
+            _testInput = new InputProperties
+            {
+                InputJson = _testJson,
+                JsonMap = _testMap
+            };
+        }
+
+        [Test]
+        public void TransformShouldAllowJTokenAsInput()
+        {
+            _testInput.InputJson = JToken.Parse(_testJson);
+            var result = JsonMapperTask.Transform(_testInput);
+        }
+
+        [Test]
+        public void TransformShouldAllowStringAsInput()
+        {
+            var result = JsonMapperTask.Transform(_testInput);
+        }
+
+        [Test]
+        public void TransformMapsStringData()
+        {
+            var result = JsonMapperTask.Transform(_testInput);
+
+            var fullName = result.ToJson()["FullName"].Value<string>();
+
+            Assert.AreEqual("Veijo Frends", fullName);
+        }
+
+        [Test]
+        public void TransformMapsNumbersCorrectly()
+        {
+            var result = JsonMapperTask.Transform(_testInput);
+
+            var age = result.ToJson()["Age"];
+
+            Assert.AreEqual(JTokenType.Integer, age.Type);
+            Assert.AreEqual(30, age.Value<int>());
+        }
+
+        [Test]
+        public void TransformationMapsBoolValueCorrectly()
+        {
+            var result = JsonMapperTask.Transform(_testInput);
+
+            var breething = result.ToJson()["StillBreething"];
+
+            Assert.AreEqual(JTokenType.Boolean, breething.Type);
+            Assert.AreEqual(false, breething.Value<bool>());
+        }
+
+
+        [Test]
+        public void InvalidJsonInputThrowsException()
+        {
+            _testInput.InputJson = @"{ foo baar";
+
+            Assert.Throws<FormatException>(() => JsonMapperTask.Transform(_testInput));
+        }
+
+        [Test]
+        public void InvalidJsonMapThrowsException()
+        {
+            _testInput.JsonMap = @"{""age"":""#valuof($.age)"", ""foo}";
+
+            Assert.Throws<Exception>(() => JsonMapperTask.Transform(_testInput));
+        }
+
+    }
+}
---
+++ Frends.Community.JsonMapper/JsonMapperTask.cs
@@ -0,0 +1,40 @@
+﻿using JUST;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Frends.Community.JsonMapper
+{
+    public class JsonMapperTask
+    {
+        /// <summary>
+        /// Maps input json using JUST.Net library. 
+        /// JsonMapper Task documentation: TODO
+        /// JUST.Net documentation: https://github.com/WorkMaze/JUST.net#just
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Object { string Result, JToken ToJson() }</returns>
+        public static TransformationResult Transform(InputProperties input)
+        {
+            string result = string.Empty;
+            //Try parse input Json for simple validation
+            try
+            {
+                JToken.Parse(input.InputJson.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new FormatException("Input Json is not valid: " + ex.Message);
+            }
+            try
+            {
+                result = JsonTransformer.Transform(input.JsonMap, input.InputJson.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Json transformation failed: " + ex.Message, ex);
+            }
+
+            return new TransformationResult(result);
+        }
+    }
+}
